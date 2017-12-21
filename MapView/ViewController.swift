d2dfc23d4fae19f7c00a97b0149395262f566697
bin0204 <seobin1026@gmail.com>
2017-12-20 14:23:39 -1000

    add extra spot

--- MapView/ViewController.swift
+++ MapView/ViewController.swift
@@ -23,33 +23,38 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         // Do any additional setup after loading the view, typically from a nib.
         lblLocationInfo1.text = "" //location info
         lblLocationInfo2.text = "" //location info
-        locationManager.delegate = self 
-        locationManager.desiredAccuracy = kCLLocationAccuracyBest
-        locationManager.requestWhenInUseAuthorization()
-        locationManager.startUpdatingLocation()
-        myMap.showsUserLocation = true
+        locationManager.delegate = self //
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest //set the accuracy to best
+        locationManager.requestWhenInUseAuthorization() //request users authorization to use access
+        locationManager.startUpdatingLocation() //start updating location
+        myMap.showsUserLocation = true //set to true
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    //get latitude and longitude
-    func goLocation(latitude latitudeValue: CLLocationDegrees, longitide longitudeValue: CLLocationDegrees, delata span :Double){
+    //get latitude,longitude,span to indicate those on map
+    func goLocation(latitude latitudeValue: CLLocationDegrees, longitide longitudeValue: CLLocationDegrees, delta span :Double) -> CLLocationCoordinate2D {
         let pLocation = CLLocationCoordinate2DMake(latitudeValue, longitudeValue)
         let spanValue = MKCoordinateSpanMake(span, span)
         let pRegion = MKCoordinateRegionMake(pLocation, spanValue)
         myMap.setRegion(pRegion, animated: true)
+        return pLocation
     }
-    //install pin
+    //install pin on specific latitude and longitude and add titles onto pin
     func setAnnotation(latitude latitudeValue: CLLocationDegrees, longitude longitudeValue: CLLocationDegrees, delta span:Double, title strTitle:String, subtitle strSubtitle:String) {
-        
+        let annotation = MKPointAnnotation() //specific point on map
+        annotation.coordinate = goLocation(latitude: latitudeValue, longitide: longitudeValue, delta: span)
+        annotation.title = strTitle
+        annotation.subtitle = strSubtitle
+        myMap.addAnnotation(annotation)
     }
-    //shows location in text
+    //shows location
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations:[CLLocation]) {
         let pLocation = locations.last
         //100 times
-        goLocation(latitude: (pLocation?.coordinate.latitude)!,longitide: (pLocation?.coordinate.longitude)!,delata: 0.01)
+        goLocation(latitude: (pLocation?.coordinate.latitude)!,longitide: (pLocation?.coordinate.longitude)!,delta: 0.01)
         CLGeocoder().reverseGeocodeLocation(pLocation!, completionHandler: {
             (placemarks, error) -> Void in
             let pm = placemarks!.first
@@ -71,6 +76,31 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         locationManager.stopUpdatingLocation()
     }
     @IBAction func sgChangeLocation(_ sender: UISegmentedControl) {
+        if sender.selectedSegmentIndex == 0 {
+            //current location
+            self.lblLocationInfo1.text = ""
+            self.lblLocationInfo2.text = ""
+            locationManager.startUpdatingLocation()
+        }
+        else if sender.selectedSegmentIndex == 1 {
+            //foodland
+            //call setAnnotation function
+            setAnnotation(latitude: 21.647257, longitude: -157.922554, delta: 0.015, title: "Foodland", subtitle: "Laie")
+            self.lblLocationInfo1.text = "Looking at"
+            self.lblLocationInfo2.text = "Foodland in Laie"
+            
+        }
+        else if sender.selectedSegmentIndex == 2 {
+            //pandaexpress
+            setAnnotation(latitude: 21.422223, longitude: -157.803559, delta: 0.015, title: "Panda Express", subtitle: "Kaneohe")
+            self.lblLocationInfo1.text = "Looking at"
+            self.lblLocationInfo2.text = "Panda Express in Kaneohe"
+        }
+        else {
+            setAnnotation(latitude: 35.230860, longitude: 129.155264, delta: 1, title: "My Home", subtitle: "한국집")
+            self.lblLocationInfo1.text = "Looking at"
+            self.lblLocationInfo2.text = "My home in South Korea"
+        }
     }
     
     
